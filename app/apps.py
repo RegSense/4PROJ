f2b2dd13b6384435ad4151cfec1381ae68bc738b
paulvalyyy <paulvalyyy@gmail.com>
2022-06-09 18:25:33 +0200

    app.settings in wsgi

--- app/apps.py
+++ app/apps.py
@@ -1,7 +1,5 @@
 from django.apps import AppConfig
-from django.apps import apps
 
-
-class AppConfig(AppConfig):
+class StatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
--- app/views.py
+++ app/views.py
@@ -18,6 +18,10 @@ def cours_eleve(request):
 
 def administration(request):
     person = "Retour base de données: " + User.objects.get(username='Abigail Byfford').first_name
+    obj_user = User.objects.all()
+    user_dic = {
+        "first_name": obj_user
+    }
     allfirstname = User.objects.all().values_list('first_name', flat=True)
     alllastname = User.objects.all().values_list('last_name', flat=True)
     allemail = User.objects.all().values_list('email', flat=True)
